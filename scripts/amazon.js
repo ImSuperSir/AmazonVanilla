bb429692edcbbb225741a2aec53f740070e22c47
imsupersirDev <imsupersir@gmail.com>
2023-11-03 01:37:29 -0700

    Use productns.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,33 +1,3 @@
-const products = [
-  {
-    image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    raiting: {
-      stars: 4.5,
-      count: 87
-    },
-    priceCents: 1090
-  },
-  {
-    image: 'images/products/intermediate-composite-basketball.jpg',
-    name: 'Intermediate Size Basketball',
-    raiting: {
-      stars: 4,
-      count: 127
-    },
-    priceCents: 2095
-  },
-  {
-    image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-    raiting: {
-      stars: 4.5,
-      count: 56
-    },
-    priceCents: 799
-  },
-];
-
 let productsHTML = '';
 
 products.forEach((pProduct) =>
@@ -43,9 +13,9 @@ products.forEach((pProduct) =>
         </div>
 
         <div class="product-rating-container">
-          <img class="product-rating-stars" src="images/ratings/rating-${pProduct.raiting.stars * 10}.png">
+          <img class="product-rating-stars" src="images/ratings/rating-${pProduct.rating.stars * 10}.png">
           <div class="product-rating-count link-primary">
-            ${pProduct.raiting.count}
+            ${pProduct.rating.count}
           </div>
         </div>
 
