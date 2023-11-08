d2cc808643f3f78610197064d1409b3137e4b6cb
imsupersirDev <imsupersir@gmail.com>
2023-11-08 00:49:58 -0800

    delete products from HTML when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -18,10 +18,10 @@ cart.forEach((cartItem) =>
     }
   });
 
-  console.log(matchingProduct);
+  //console.log(matchingProduct);
 
   cartSummaryHTML +=
-    `<div class="cart-item-container">
+    `<div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
       <div class="delivery-date">
         Delivery date: Tuesday, June 21
       </div>
@@ -109,7 +109,12 @@ document.querySelectorAll('.js-delete-quantity-link')
 
       const { productId } = link.dataset;  //with destructuration using the curly brackets, const productId = link.dataset.productId  <= this is the normal form
       removeFromCart(productId);
-      console.log(cart);
+
+      const container = document.querySelector(`.js-cart-item-container-${productId}`);
+
+      // console.log(container);
+
+      container.remove();
 
     });
   });
